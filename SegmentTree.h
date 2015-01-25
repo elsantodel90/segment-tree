945712038a5860f29935816a66c1d5a7eb60eaf3
Agustin Santiago Gutierrez <elsantodel90@gmail.com>
2015-01-25 19:29:15 -0300

    Emprolijamiento de ejemplos, marcado de lineas SLU, retoques

--- SegmentTree.h
+++ SegmentTree.h
@@ -18,16 +18,27 @@
 //  -->  stat.update(UpdateData()) == stat
 //  --> (stat.update(data1)).update(data2) == stat.update(data1.merge(data2))
 
+// Anotadas con el comentar SLU ("Solo Lazy Update") van lineas que se comentan si uno no va a usar Lazy Update.
+// Es altamente recomendado volarlas para ahorrar memoria y tiempo de ejecucion.
+// Cuando se comenta una funcion como split, la idea es que hay que volar la funcion entera.
+// Si uno se fija, en lugar de un arreglo de Node en tal caso podria tener directamente el arreglo de stat, y llamar sin intermediario
+// a las funciones de stat. Pero si uno no quiere pensar, simplemente omitir las SLU funciona, y deberia tener un impacto en eficiencia
+// minimo, porque el compilador se supone que se re aviva de optimizar esas cosas.
+
 template <typename Stats, typename UpdateData>
 struct SegmentTree
 {
     struct Node
     {
-        UpdateData lazy;
+        UpdateData lazy; // SLU
         Stats stat;
         void merge(const Node &a, const Node &b) {stat = Stats(a.stat,b.stat);}
-        void update(int i, int j, const UpdateData &data) {lazy.update(i,j,data); stat.update(i,j,data);}
-        void split(int i, int j, int c, Node &a, Node &b)
+        void update(int i, int j, const UpdateData &data)
+        {
+            lazy.update(i,j,data); // SLU
+            stat.update(i,j,data);
+        }
+        void split(int i, int j, int c, Node &a, Node &b) // SLU
         {
             a.update(i,c, lazy);
             b.update(c,j, lazy);
@@ -47,7 +58,7 @@ struct SegmentTree
         if (qA <= a && b <= qB) return NODE.stat;
         if (qB <= a || b <= qA) return Stats();
         int c = (a+b)/2;
-        NODE.split(a,b,c, LEFT, RIGHT);
+        NODE.split(a,b,c, LEFT, RIGHT); // SLU
         return Stats(iget(a, c, 2*i), iget(c, b, 2*i+1));
     }
     void iupdate(int a, int b, int i) // Update interno
@@ -55,7 +66,7 @@ struct SegmentTree
         if (qA <= a && b <= qB) {NODE.update(a,b, qData); return ;}
         if (qB <= a || b <= qA) return;
         int c = (a+b)/2;
-        NODE.split(a,b,c, LEFT, RIGHT);
+        NODE.split(a,b,c, LEFT, RIGHT); // SLU
         iupdate(a, c, 2*i);
         iupdate(c, b, 2*i+1);
         NODE.merge(LEFT, RIGHT);
@@ -63,8 +74,8 @@ struct SegmentTree
     // Funciones que puede llamar el usuario (public)
     void init() {dforsn(i,1, N) NODE.merge(LEFT, RIGHT);} // Se inicializa luego de llenar el arreglo userData con los valores iniciales.
     Stats get(int a,int b) {qA = a; qB = b; return iget(0, N, 1); } // Get que llama el usuario
-    void updateRange(int a,int b, const UpdateData &data) {qData = data; qA = a; qB = b; iupdate(0, N, 1);} // updateRange que puede llamar el usuario
-    void update(int i, const UpdateData &data) { updateRange(i,i+1, data); } // update que puede llamar el usuario
+    void updateRange(int a,int b, const UpdateData &data) {qData = data; qA = a; qB = b; iupdate(0, N, 1);} // updateRange que puede llamar el usuario: SLU
+    void update(int i, const UpdateData &data) { qData = data; qA = i; qB = i+1; iupdate(0, N, 1); } // update que puede llamar el usuario
 };
 
 
