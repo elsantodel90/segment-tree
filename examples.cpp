2c22f17f6bf80fe599c446ad1b77b1980f62e31c
Agustin Santiago Gutierrez <elsantodel90@gmail.com>
2015-01-26 19:30:38 -0300

    Fixed examples

--- examples.cpp
+++ examples.cpp
@@ -95,12 +95,12 @@ struct HackerCupStat
 // varias lineas en la implementacion del segment tree, y no haria falta la operacion update de SumData, ni el constructor default del mismo.
 // Con lo cual en ese caso podriamos usar de sumData directamente a un int, lo cual es comodo porque ni hay que programar este structcito.
 
-struct SumUpdateData
+struct SumData
 {
     int lazy;
-    SumUpdateData() : lazy(0) {}
-    SumUpdateData(int val) : lazy(val) {}
-    void update(int i,int j, const SumUpdateData &o) {lazy += o.lazy;};
+    SumData() : lazy(0) {}
+    SumData(int val) : lazy(val) {}
+    void update(int i,int j, const SumData &o) {lazy += o.lazy;}; // Esta solo hace falta cuando usamos Lazy Update
 };
 
 
@@ -109,8 +109,11 @@ struct SumStat
     int s;
     SumStat() : s(0) {}
     SumStat(int val) : s(val) {}
-    SumStat(const SumStat &a, const SumStat &b) : s(a.s + b.s) {}
-    void update(int i,int j, const SumUpdateData &data) {s += data.lazy * (j-i);}
+    SumStat(const SumStat &a, const SumStat &b) {merge(a,b);}
+    void merge(const SumStat &a, const SumStat &b, const SumData &data = 0) {
+        s = a.s + b.s;
+    }
+    void update(int i,int j, const SumData &data) {s += data.lazy * (j-i);} // Podria ser simplemente s += data.lazy si no usamos lazyUpdate, ya que en ese caso j-i == 1 siempre.
 };
 
 // Ejemplo de uso.
@@ -120,8 +123,8 @@ void ejemploSumaConLazyUpdate()
     // Notar que al haberle puesto un constructor que toma int a LazySumStat y LazySumData, podemos asignarle ints de una,
     //  lo cual nos limpia un poco la sintaxis en este ejemplo pavo donde los structs no son nada mas que un int.
     int N = 10;
-    SegmentTree<SumStat, SumUpdateData>::Node *v;
-    SegmentTree<SumStat, SumUpdateData> tree(N, v); // En esta version, todo bien con la memoria local de stack
+    SegmentTree<SumStat, SumData>::Node *v;
+    SegmentTree<SumStat, SumData> tree(N, v); // En esta version, todo bien con la memoria local de stack
     forn(i,N) v[i].stat = i*i; // Se inicializa el v[i].stat, y los datos lazy se dejan como estan.
     tree.init();
     // Impresion del estado interno completo, a modo de DEBUG de que se construyo bien.
@@ -141,6 +144,9 @@ void ejemploSumaConLazyUpdate()
         cout << i << "   " << tree.v[i].stat.s << " " << tree.v[i].lazy.lazy << endl;
 }
 
+// Para que compile este, hay que comentar las lineas correspondientes en el codigo de SegmentTree
+
+/*
 void ejemploSumaComunSinLazyUpdate()
 {
     // Notar que al haberle puesto un constructor que toma int a LazySumStat y LazySumData, podemos asignarle ints de una,
@@ -166,10 +172,10 @@ void ejemploSumaComunSinLazyUpdate()
     forn(i,tree.N*2)
         cout << i << "   " << tree.v[i].stat.s << endl;
 }
-
+*/
 int main()
 {
-    ejemploSumaComunSinLazyUpdate(); // Para que compile este, hay que comentar las lineas correspondientes en el codigo de SegmentTree
+    //ejemploSumaComunSinLazyUpdate(); // Para que compile este, hay que comentar las lineas correspondientes en el codigo de SegmentTree
     ejemploSumaConLazyUpdate();
     return 0;
 }
